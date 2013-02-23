cc5c7598ee0011b4c3d443d1703dd6acb7657602
Lukasz Strzelecki <lu.strzelecki@gmail.com>
2013-02-23 09:22:50 +0100

    initial commit

---
+++ src/main/java/com/androidkickstarter/app/HomeActivity.java
@@ -0,0 +1,27 @@
+
+package com.androidkickstarter.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class HomeActivity
+        extends Activity {
+
+    private TextView hello;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hello = ((TextView) findViewById(R.id.hello));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater();
+        return true;
+    }
+
+}
