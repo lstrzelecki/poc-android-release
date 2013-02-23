ab5a10547caddd85a4a3efbadb3bb3528218172b
Lukasz Strzelecki <lu.strzelecki@gmail.com>
2013-02-23 09:35:45 +0100

    display parameters

--- src/main/java/com/androidkickstarter/app/HomeActivity.java
+++ src/main/java/com/androidkickstarter/app/HomeActivity.java
@@ -2,20 +2,36 @@
 package com.androidkickstarter.app;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
+
+import static java.lang.String.format;
 
 public class HomeActivity
         extends Activity {
 
-    private TextView hello;
+    private TextView versionName;
+    private TextView versionCode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hello = ((TextView) findViewById(R.id.hello));
+        versionName = ((TextView) findViewById(R.id.version_name));
+        versionCode = (TextView) findViewById(R.id.version_code);
+
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName.setText(format("Version name: %s" ,packageInfo.versionName));
+            versionCode.setText(format("Version code: %s" ,packageInfo.versionCode));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
     @Override
